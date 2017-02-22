a7c607f4797c6f8c13ebfc8125d78438b48224f5
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-02-22 18:09:24 -0500

    set files

---
+++ VR_Week4/Assets/scripts/RandomRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 90);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
